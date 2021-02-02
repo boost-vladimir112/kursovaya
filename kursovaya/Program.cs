b017e2913d725c77f0b6dac3edb666d857d1bcd8
AndreyZ <boost.vladimir112@gmail.com>
2021-02-02 16:50:35 +0300

    Revert "changed 02.02"
    This reverts commit 574d1127468a5bfe2c2af25cd4a6782e167f961b.

--- kursovaya/Program.cs
+++ kursovaya/Program.cs
@@ -17,7 +17,6 @@ namespace kursovaya
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new new_base_comp());
-            /// privet
         }
     }
 }
